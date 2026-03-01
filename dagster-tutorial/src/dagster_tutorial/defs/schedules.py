8e6e51a83ba06c38576951ee04649de85b7cef72
Nick Brett <nick.brett1@gmail.com>
2026-03-01 19:50:46 +0000

    Added schedule

---
+++ dagster-tutorial/src/dagster_tutorial/defs/schedules.py
@@ -0,0 +1,6 @@
+import dagster as dg
+
+
+@dg.schedule(cron_schedule="0 0 * * *", target="*")
+def tutorial_schedule(context: dg.ScheduleEvaluationContext) -> dg.RunRequest | dg.SkipReason:
+    return dg.SkipReason("Skipping. Change this to return a RunRequest to launch a run.")
